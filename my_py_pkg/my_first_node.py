5e37b6a6c051c5e35e79a23b21b39c596b2fd933
beyzatezcan <beyzanurtezcan424@gmail.com>
2024-10-29 14:19:10 +0000

    Add OOP version of my_first_node.py

--- my_py_pkg/my_first_node.py
+++ my_py_pkg/my_first_node.py
@@ -1,11 +1,22 @@
 import rclpy #ros2 kutuphanesi
 from rclpy.node import Node
+
+class MyNode(Node):
+    def __init__(self):
+            super().__init__("python_test")
+            self.counter_ = 0 
+            self.get_logger().info("hello ros2")
+            self.create_timer(0.5,self.timer_callback)
+    def timer_callback(self):
+            self.counter_+=1
+            self.get_logger().info(f"welcome{self.counter_}")
+            
+
 def main(args=None):
-    rclpy.init(args=args)
-    node=Node("python_test")
-    node.get_logger().info("hello ros2")
+    rclpy.init(args = args)
+    node=MyNode()
     rclpy.spin(node) #bitmemesi icin spine alinir ve ctrl+c ile cikilir
     rclpy.shutdown() #bitirir
-if __name__=="__main__" :
- main()
+if __name__== "__main__" :
+    main()
  #colcon build ederken colcon build --packages-select dosyaadi yaparsak tek dosyayi build ederiz
